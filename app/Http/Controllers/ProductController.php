8a8b30887d67e8be8c268b165a84b38a91c9e79a
Piero <piero.onorati@virgilio.it>
2021-09-08 18:55:41 +0200

    create product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Trip;
 
 class ProductController extends Controller
 {
     public function products() {
-        return view('products');
+        $trips = Trip::paginate(12);
+        return view('products', compact('trips'));
     }
 
 }
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,5 +3,41 @@
 
 @section('content')
     
-<h2>This is the product page</h2>
+<div class="container">
+    <h2 class="py-5">Choose your next adventure...</h2>
+</div>
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+        @foreach ($trips as $trip)
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h4 class="card-title">{{$trip['name']}}</h4>
+
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-text">
+                            <img src="{{asset('img/place.png')}}" style="width: 30px;" alt="place icon">
+                            <span class="ps-3">City:</span> {{$trip['city']}}
+                        </h5>
+                        <h6>
+                            <img src="{{asset('img/people.png')}}" style="width: 30px;" alt="people icon">
+                            <span class="ps-3">People:</span> {{$trip['people']}}
+                        </h6>
+                        <a href="#" class="btn btn-primary mt-2">More Details</a>
+                    </div>
+                    <div class="card-footer text-muted text-center">
+                        <h5>Price: {{$trip['price']}} $</h5>
+                    </div>
+        
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="py-4">
+        {{$trips->links()}}
+    </div>
+</div>
 @endsection
