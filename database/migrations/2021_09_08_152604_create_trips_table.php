8ddfd5f68e6cf9523c51a0b857efcf19af4281ef
Piero <piero.onorati@virgilio.it>
2021-09-08 17:37:39 +0200

    make migration

---
+++ database/migrations/2021_09_08_152604_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('city', 50);
+            $table->text('description');
+            $table->string('type')->default('romantic');
+            $table->tinyInteger('people')->unsigned();
+            $table->float('price', 6, 2);
+            $table->date('validity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
